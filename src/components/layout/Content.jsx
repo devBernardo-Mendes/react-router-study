a69815c00ca27b4d006ac8937e3783ca02bab82e
bernardo-mr1 <devbernardomr@gmail.com>
2021-09-21 17:58:13 -0300

    feat: :sparkles: Construction of application navigation and component route

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-import './Content.css'
+import './Content.css';
+import { Switch, Route } from 'react-router-dom'
+import About from '../../views/examples/About'
+import Home from '../../views/examples/Home'
 
 const Content = props => (
     <main className="Content">
-        components Content
+        <Switch>
+            <Route path="/about">
+                <About />
+            </Route>
+            <Route path="/home">
+                <Home />
+            </Route>
+        </Switch>
     </main>
 )
 
